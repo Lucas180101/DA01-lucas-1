e4f3ab0724be6263b0e67cc17bb507be5b5fccfe
Lucas180101 <147169152+Lucas180101@users.noreply.github.com>
2023-10-17 11:35:56 +0700

    Update Day9_Practice.sql

--- Day9_Practice.sql
+++ Day9_Practice.sql
@@ -8,3 +8,18 @@ SELECT *,
 IF(x + y > z AND x + z > y AND y + z > x, 'Yes', 'No') AS triangle
 FROM Triangle;
 --bài tập 3
+SELECT
+ROUND(100.0 * 
+SUM(CASE WHEN call_category IS NULL OR call_category = 'n/a'
+THEN 1 ELSE 0 END)/COUNT(call_category), 1) AS call_percentage
+FROM callers;
+-- bài tập 4 
+select name from (select name, COALESCE(referee_id,0) as 'referee_id' from customer) AS referred_List
+where referee_id <> 2
+-- bài 5
+SELECT survived,
+SUM(CASE WHEN pclass = 1 THEN 1 ELSE 0 END) AS first_class,
+SUM(CASE WHEN pclass = 2 THEN 1 ELSE 0 END) AS second_class,
+SUM(CASE WHEN pclass = 3 THEN 1 ELSE 0 END) AS third_class
+FROM titanic
+GROUP BY survived;
