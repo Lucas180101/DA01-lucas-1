760e89b80fdbd72fafd3ca683e466684df12e09b
Lucas180101 <147169152+Lucas180101@users.noreply.github.com>
2023-11-07 18:01:48 +0700

    Update Day 21-22 _Practice.sql

--- Day 21-22 _Practice.sql
+++ Day 21-22 _Practice.sql
@@ -1,22 +1,52 @@
 -- TASK Ad_HOC
 -- bài tập 1
-SELECT EXTRACT(year FROM shipped_at) || '-' || EXTRACT(month FROM shipped_at) AS month_year,
+SELECT EXTRACT(year FROM delivered_at) || '-' || EXTRACT(month FROM delivered_at) AS month_year,
        COUNT(DISTINCT user_id) AS total_user,
        COUNT(order_id) AS total_order
        
 FROM bigquery-public-data.thelook_ecommerce.orders
-WHERE shipped_at BETWEEN '2019-01-01'AND '2022-04-30'
+WHERE delivered_at BETWEEN '2019-01-01'AND '2022-04-30'
 GROUP BY month_year
 ORDER BY total_order DESC
 -- bài tập 2 
+SELECT EXTRACT(year FROM delivered_at) || '-' || EXTRACT(month FROM delivered_at) AS month_year,
+       COUNT(DISTINCT user_id) AS total_user,
+       COUNT(order_id) AS total_order
+       
+FROM bigquery-public-data.thelook_ecommerce.orders
+WHERE delivered_at BETWEEN '2019-01-01'AND '2022-04-30'
+GROUP BY month_year
+ORDER BY total_order DESC
+
+       cte_1 AS(SELECT * FROM bigquery-public-data.thelook_ecommerce.orders AS a 
+INNER JOIN bigquery-public-data.thelook_ecommerce.order_items AS b 
+ON a.order_id=b.order_id)
+
+SELECT 
+FROM cte_1
+
+WITH monthly_summary AS (
+  SELECT
+    FORMAT_TIMESTAMP('%Y-%m', oi.delivered_at) AS month_year,
+    COUNT(DISTINCT o.user_id) AS distinct_users,
+    SUM(oi.sale_price) AS total_sale_price,
+    COUNT(DISTINCT o.order_id) AS total_orders
+  FROM
+    bigquery-public-data.thelook_ecommerce.orders AS o
+  INNER JOIN
+    bigquery-public-data.thelook_ecommerce.order_items AS oi
+  ON
+    o.order_id = oi.order_id
+  WHERE
+    oi.delivered_at BETWEEN '2019-01-01' AND '2022-04-30'
+  GROUP BY
+    month_year
+)
 SELECT
-  EXTRACT(year FROM a.shipped_at) || '-' || EXTRACT(month FROM a.shipped_at) AS month_year,
-  COUNT(DISTINCT a.user_id) AS distinct_user,
-  AVG(b.sale_price) OVER (PARTITION BY EXTRACT(year FROM a.shipped_at)) AS avg_sale_price
+  month_year,
+  distinct_users,
+  total_sale_price / total_orders AS average_order_value
 FROM
-  bigquery-public-data.thelook_ecommerce.orders AS a
-INNER JOIN
-  bigquery-public-data.thelook_ecommerce.order_items AS b 
-  ON a.order_id = b.order_id
-WHERE
-  a.shipped_at BETWEEN '2019-01-01' AND '2022-04-30'
+  monthly_summary
+ORDER BY
+  month_year;
