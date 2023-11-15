086a1e78b88c91bc4289205a7a8de7c8257679a3
Lucas180101 <147169152+Lucas180101@users.noreply.github.com>
2023-11-15 16:18:14 +0700

    Update Update Day22_Practice.sql

--- Update Day22_Practice.sql
+++ Update Day22_Practice.sql
@@ -1,4 +1,3 @@
-CREATE OR REPLACE VIEW  vw_ecommerce_analyst AS(
 WITH cte AS (
   SELECT 
     EXTRACT(YEAR FROM a.created_at) AS year, 
@@ -26,7 +25,7 @@ main_data AS (
 
 cte2 AS (
   SELECT 
-    year || '-' || month AS Month,   
+    TIMESTAMP(CONCAT(CAST(year AS STRING), '-', CAST(month AS STRING), '-01')) AS Month,   
     year AS Year,
     Product_category,
     SUM(Sale) AS TPV,
@@ -35,18 +34,60 @@ cte2 AS (
   FROM main_data cte
   GROUP BY Month, Product_category, Year
 )
+,Ecommerce_index AS (
+  SELECT 
+    TPO,
+    TPV,
+    Month,
+    FORMAT_DATE('%Y-%m', Month) AS cohort_date,
+    (EXTRACT(YEAR FROM Month) - EXTRACT(YEAR FROM first_purchase_date)) * 12
+      + EXTRACT(MONTH FROM Month) - EXTRACT(MONTH FROM first_purchase_date) + 1 AS index
+  FROM (
+    SELECT 
+      Month,
+      Year,
+      Product_category,
+      TPV,
+      TPO,
+      Total_Cost,
+      TPV - Total_Cost AS Total_profit,
+      (TPV - Total_Cost) / Total_Cost AS Profit_to_cost_ratio,
+      LEAD(TPV) OVER (PARTITION BY Month ORDER BY TPV) AS Next_Sale,
+      ROUND((TPV - LEAD(TPV) OVER (PARTITION BY Month ORDER BY TPV)) / TPV * 100, 2) AS Revenue_growth,
+      LEAD(TPO) OVER (PARTITION BY Month ORDER BY TPO) AS Next_Order,
+      ROUND((TPO - LEAD(TPO) OVER (PARTITION BY Month ORDER BY TPO)) / TPO * 100, 2) AS Order_growth,
+      MIN(Month) OVER (PARTITION BY Year) AS first_purchase_date
+    FROM cte2
+  ) AS subquery
+),
+xxx AS (
+  SELECT 
+    TPV,
+    TPO,
+    cohort_date,
+    index
+  FROM Ecommerce_index
+  GROUP BY cohort_date, index, TPV, TPO
+)
 
-SELECT 
-  Month,
-  Year,
-  Product_category,
-  TPV,
-  TPO,
-  Total_Cost,
-  TPV - Total_Cost AS Total_profit,
-  (TPV - Total_Cost) / Total_Cost AS Profit_to_cost_ratio,
-  LEAD(TPV) OVER (PARTITION BY Month ORDER BY TPV) AS Next_Sale,
-  ROUND((TPV - LEAD(TPV) OVER (PARTITION BY Month ORDER BY TPV)) / TPV * 100, 2)||'%' AS Revenue_growth,
-  LEAD(TPO) OVER (PARTITION BY Month ORDER BY TPO) AS Next_Order,
-  ROUND((TPO - LEAD(TPO) OVER (PARTITION BY Month ORDER BY TPO)) / TPO * 100, 2)||'%' AS Order_growth
-FROM cte2);
+-- customer_cohort
+,customer_cohort AS (
+  SELECT 
+    cohort_date,
+    SUM(CASE WHEN index = 1 THEN TPO ELSE 0 END) AS m1,
+    SUM(CASE WHEN index = 2 THEN TPO ELSE 0 END) AS m2,
+    SUM(CASE WHEN index = 3 THEN TPO ELSE 0 END) AS m3,
+    SUM(CASE WHEN index = 4 THEN TPO ELSE 0 END) AS m4
+  FROM xxx
+  GROUP BY cohort_date
+  ORDER BY cohort_date
+)
+
+-- retention cohort
+SELECT
+  cohort_date,
+  (100 - ROUND(100.00 * m1 / NULLIF(m1, 0), 2)) || '%' AS m1,
+  (100 - ROUND(100.00 * m2 / NULLIF(m1, 0), 2)) || '%' AS m2,
+  (100 - ROUND(100.00 * m3 / NULLIF(m1, 0), 2)) || '%' AS m3,
+  ROUND(100.00 * m4 / NULLIF(m1, 0), 2) || '%' AS m4
+FROM customer_cohort;
